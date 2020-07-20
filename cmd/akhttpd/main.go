ef7bf7ddec8e286b3a96fa5f7ce2f32b1b4e489c
Tom Wiesing <tkw01536@gmail.com>
2020-07-20 21:17:00 +0200

    Move command into structs

--- cmd/akhttpd/main.go
+++ cmd/akhttpd/main.go
@@ -3,8 +3,32 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"time"
+
+	"github.com/tkw1536/akhttpd"
 )
+
+func main() {
+	r, err := akhttpd.NewGitHubKeyRepository(akhttpd.GHKeyRepositoryOptions{
+		GitHubToken:     token,
+		UpstreamTimeout: time.Duration(flagTimeout) * time.Second,
+		MaxCacheSize:    flagCacheBytes,
+		MaxCacheAge:     flagCacheTimeout,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// setup handler
+	http.HandleFunc("/", akhttpd.NewHandler(r, akhttpd.HandlerOpts{}))
+
+	// bind
+	log.Printf("Listening on %s\n", bindAddress)
+	http.ListenAndServe(bindAddress, nil)
+}
 
 // args contains the command line arguments
 var args []string
@@ -14,11 +38,10 @@ var flagCacheTimeout int64
 var flagTimeout int
 var bindAddress string
 
-func initFlags() {
-
+func init() {
 	flag.StringVar(&token, "token", os.Getenv("GITHUB_TOKEN"), "token for github authentication (can also be set by 'GITHUB_TOKEN' variable). ")
 	flag.Int64Var(&flagCacheBytes, "cache-size", 25*1000, "maximum in-memory cache size in bytes")
-	flag.Int64Var(&flagCacheBytes, "cache-age", 60*60, "maximum number of seconds after which cache entries should expire")
+	flag.Int64Var(&flagCacheTimeout, "cache-age", 60*60, "maximum number of seconds after which cache entries should expire")
 	flag.IntVar(&flagTimeout, "timeout", 1, "timeout in seconds after which to expire akhttpd")
 	flag.Parse()
 
--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.14
 
 require (
 	github.com/die-net/lrucache v0.0.0-20190707192454-883874fe3947
+	github.com/google/go-github v17.0.0+incompatible
 	github.com/google/go-github/v29 v29.0.3
 	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79
+	github.com/pkg/errors v0.9.1
 	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
 )
