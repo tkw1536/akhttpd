01662618f725822fc1c1dd16ac2a7f41dc2070ce
Tom Wiesing <tkw01536@gmail.com>
2024-04-22 01:57:48 +0200

    Use pkglib instead of custom packages

--- go.mod
+++ go.mod
@@ -5,16 +5,17 @@ go 1.22.2
 require (
 	github.com/die-net/lrucache v0.0.0-20220628165024-20a71bc65bf1
 	github.com/google/go-github v17.0.0+incompatible
-	github.com/gorilla/websocket v1.5.1
 	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79
 	github.com/mpolden/echoip v0.0.0-20230521182614-d84665c26cf7
 	github.com/pkg/errors v0.9.1
+	github.com/tkw1536/pkglib v0.0.0-20240421230152-7fcf00edc7df
 	golang.org/x/crypto v0.22.0
 	golang.org/x/oauth2 v0.19.0
 )
 
 require (
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
 	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 )
--- pkg/repo/uploadable.go
+++ pkg/repo/uploadable.go
@@ -2,6 +2,7 @@ package repo
 
 import (
 	"context"
+	"crypto/rand"
 	"fmt"
 	"io"
 	"net/http"
@@ -9,12 +10,14 @@ import (
 	"sync/atomic"
 
 	"github.com/pkg/errors"
-	"github.com/tkw1536/akhttpd/pkg/password"
-	"github.com/tkw1536/akhttpd/pkg/wshandler"
 
 	"golang.org/x/crypto/ssh"
 
 	_ "embed"
+
+	"github.com/tkw1536/pkglib/lazy"
+	"github.com/tkw1536/pkglib/password"
+	"github.com/tkw1536/pkglib/websocketx"
 )
 
 // spellchecker:words akhttpd wshandler userkeys
@@ -30,6 +33,8 @@ type UploadableKeys struct {
 
 	lock sync.RWMutex
 	data map[string][]ssh.PublicKey
+
+	server lazy.Lazy[*websocketx.Server]
 }
 
 var errUserKeysNotConfigured = UserNotFoundError{errors.New("User is not configured in UserKeys")}
@@ -80,11 +85,24 @@ func (uk *UploadableKeys) Register(keys ...ssh.PublicKey) (username string, clea
 
 // username generates a new username
 func (uk *UploadableKeys) username() string {
-	hash, err := password.Password(10)
+	hash, err := password.Generate(rand.Reader, 10, password.DefaultCharSet)
 	if err != nil { // fallback to a counter-based approach
 		return fmt.Sprintf("%s%d", uk.Prefix, atomic.AddUint64(&uk.counter, 1))
 	}
 	return uk.Prefix + hash
+}
+
+func (uk *UploadableKeys) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !uk.auth(w, r) {
+		return
+	}
+
+	uk.server.Get(func() *websocketx.Server {
+		return &websocketx.Server{
+			Handler:  uk.handleWS,
+			Fallback: http.HandlerFunc(uk.handleHTTP),
+		}
+	}).ServeHTTP(w, r)
 }
 
 //go:embed uploadable.min.html
@@ -112,39 +130,31 @@ func (uk *UploadableKeys) auth(w http.ResponseWriter, r *http.Request) bool {
 	return false
 }
 
-func (uk *UploadableKeys) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if !uk.auth(w, r) {
+func (uk *UploadableKeys) handleWS(conn *websocketx.Connection) {
+	key, ok := <-conn.Read()
+	if !ok {
 		return
 	}
 
-	// if an upgrade to the websocket was requested, serve a websocket!
-	if r.Header.Get("Upgrade") == "websocket" {
-		wshandler.Handle(w, r, func(messenger wshandler.WebSocket) {
-			key, ok := messenger.Read() // wait for any kind of message
-			if !ok {
-				return
-			}
-
-			// read a private key from the connection!
-			pk, _, _, _, err := ssh.ParseAuthorizedKey([]byte(key))
-			if err != nil {
-				return
-			}
-
-			// register the key
-			username, cleanup := uk.Register(pk)
-			defer cleanup()
-
-			// write the username back!
-			if !messenger.Write(username) {
-				return
-			}
-
-			messenger.Wait()
-		})
+	// read a private key from the connection!
+	pk, _, _, _, err := ssh.ParseAuthorizedKey(key.Body)
+	if err != nil {
 		return
 	}
 
+	// register the key
+	username, cleanup := uk.Register(pk)
+	defer cleanup()
+
+	// Write the username back
+	<-conn.WriteText(username)
+
+	// and wait for the connection to be closed
+	// by the client
+	<-conn.Context().Done()
+}
+
+func (uk *UploadableKeys) handleHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
 	w.Write(uploadableHTML)
